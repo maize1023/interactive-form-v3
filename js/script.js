02318ebbb0fbfaff4cfb3b5d5398295a5b162c70
Daiki Iwata <direction2423@gmail.com>
2023-03-02 14:51:05 -0800

    other job roll display none/block

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-// default focus
 // デフォルトでフォーカスを当てる
-var element = document.querySelector('#name');
-element.focus();
+const nameInput = document.querySelector('#name');
+nameInput.focus();
+
+// otherが押されたら、other job roleを表示
+const otherJobRole = document.getElementById('other-job-role');
+const jobOptions = document.getElementById('title');
+
+
+
+
+// option達の親要素である、selectをidで取得して、changeで監視しchangeされたら、displayイベントを追加
+jobOptions.addEventListener('change', ()=>{
+  if(jobOptions.value === 'other'){
+    otherJobRole.style.display = 'block';
+  }else{
+    otherJobRole.style.display = 'none';
+  }
+});
